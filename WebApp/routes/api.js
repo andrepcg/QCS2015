928bdf1aef641f2c75ec5e57382b078b008dafe3
andrepcg <andrepcg@gmail.com>
2015-05-26 21:17:33 +0100

    sei la

--- WebApp/routes/api.js
+++ WebApp/routes/api.js
@@ -21,6 +21,13 @@ var wsdl_sources = [
 var soap = require("../voter");
 var NVoter = new soap(wsdl_sources);
 
+router.get("/history", function(req, res, next){
+    res.json({
+        physicalActivitySamples: req.session.physicalActivitySamples,
+        bloodSugarDropSamples: req.session.bloodSugarDropSamples
+    });
+});
+
 
 router.get('/mealtime_insulin_dose', function(req, res, next) {
 
@@ -30,28 +37,42 @@ router.get('/mealtime_insulin_dose', function(req, res, next) {
     var a2 = req.query.carbohydrateToInsulinRatio;
     var a3 = req.query.preMealBloodSugar;
     var a4 = req.query.targetBloodSugar;
-    var a5 = req.query.personalSensitivity;
+    //var a5 = req.query.personalSensitivity;
 
     var a6 = req.query.physicalActivitySamples;
     var a7 = req.query.bloodSugarDropSamples;
     var a8 = req.query.physicalActivityLevel;
 
-    if(session.physicalActivitySamples == undefined)
-        session.physicalActivitySamples = [a6];
-    else
-        session.physicalActivitySamples.push(a6);
+    if(session.physicalActivitySamples == undefined){
+        session.physicalActivitySamples = [];
+        for (var i = 0, len = a6.length; i < len; i++)
+            session.physicalActivitySamples.push(Number(a6[i]));
+    }
+    else{
+        //session.physicalActivitySamples.push(a6);
+        for (var i = 0, len = a6.length; i < len; i++)
+            session.physicalActivitySamples.push(Number(a6[i]));
+    }
 
-    if(session.bloodSugarDropSamples == undefined)
-        session.bloodSugarDropSamples = [a7];
-    else
-        session.bloodSugarDropSamples.push(a7);
+    if(session.bloodSugarDropSamples == undefined){
+        session.bloodSugarDropSamples = [];
+        for (var i = 0, len = a7.length; i < len; i++)
+            session.bloodSugarDropSamples.push(Number(a7[i]));
+    }
+    else{
+        for (var i = 0, len = a7.length; i < len; i++)
+            session.bloodSugarDropSamples.push(Number(a7[i]));
+        //session.bloodSugarDropSamples.push(a7);
+    }
 
     console.log(session.physicalActivitySamples);
+    console.log(session.bloodSugarDropSamples);
 
     NVoter.getPersonalSensitivityToInsulin(a8, session.physicalActivitySamples, session.bloodSugarDropSamples, function(personalSensitivity){
-        //res.json(r);
+        console.log("Personal activity level", a8)
+        console.log("personalSensitivity", personalSensitivity.result);
 
-        NVoter.getMealtimeInsulinDose(a1, a2, a3, a4, personalSensitivity, function(r){
+        NVoter.getMealtimeInsulinDose(a1, a2, a3, a4, personalSensitivity.result, function(r){
             res.json(r);
         }); 
     });
